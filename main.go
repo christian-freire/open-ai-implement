c408ae7c792d5a4a466b42df45708a91f1393239
Christian Evangelista Freire <74272121+christian-freire@users.noreply.github.com>
2023-07-03 15:42:40 -0300

    [feat] Final implementation commit

--- main.go
+++ main.go
@@ -6,21 +6,24 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/spf13/viper"
 )
 
-type APIResponse struct {
-	ID      string `json:"id"`
-	Model   string `json:"model"`
-	Choices []struct {
-		Text         string `json:"text"`
-		FinishReason string `json:"finish_reason"`
-	} `json:"choices"`
-}
-
 func main() {
-	apiKey := "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"
 
-	input := "Qual a origem da guitarra Gibson Les Paul?"
+	viper.AutomaticEnv()
+
+	viper.SetConfigFile("configs/env.yml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Erro ao ler arquivo de configuração:", err.Error())
+		return
+	}
+
+	apiKey := viper.GetString("apiKey")
+
+	input := "Vale a pena um notebook mac para programar?"
 
 	requestBody, err := json.Marshal(map[string]interface{}{
 		"model":      "text-davinci-003",
@@ -54,16 +57,5 @@ func main() {
 		return
 	}
 
-	var apiResponse APIResponse
-	err = json.Unmarshal(body, &apiResponse)
-	if err != nil {
-		fmt.Println("Erro ao analisar resposta JSON: ", err.Error())
-		return
-	}
-
-	if len(apiResponse.Choices) > 0 {
-		fmt.Println(apiResponse.Choices[0].Text)
-	} else {
-		fmt.Println("Nenhuma resposta encontrada.")
-	}
+	fmt.Println(string(body))
 }
